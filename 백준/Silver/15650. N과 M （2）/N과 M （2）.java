302964ade74e90daf4cf064ab20d399a85f9011f
CheonBi <122432728+CheonBi@users.noreply.github.com>
2024-07-23 22:11:42 +0900

    [Silver III] Title: N과 M (2), Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visited = new boolean[N];
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int start, int depth){
+        if(depth == M){
+            for(int num : arr){
+                sb.append(num).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i<N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                arr[depth] = i + 1;
+                dfs(i,depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
