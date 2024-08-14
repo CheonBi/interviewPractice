2b9979d5b95f7f356e98b089360d6a685cbeccd8
CheonBi <122432728+CheonBi@users.noreply.github.com>
2024-08-14 15:04:05 +0900

    [D3] Title: 햄버거 다이어트, Time: 177 ms, Memory: 20,032 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.util.*;
 
 /**
- * SWEA No.5215 햄버거 다이어트
- * (사실은 햄버거를 안먹는게 다이어트이다...)
+ * SWEA No.5215 햄버거 다이어트 (사실은 햄버거를 안먹는게 다이어트이다...)
  *
  */
 
@@ -14,72 +13,64 @@ import java.util.*;
 
 public class Solution {
 
-	static int[][] ingredients;
+	static int[] scores;
+	static int[] kcals;
 	static boolean[] isUsed;
 	static int result;
 	static int N, L;
-	
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
-		
+
 		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1; tc<=T; tc++) {
-			
+
+		for (int tc = 1; tc <= T; tc++) {
+
 			st = new StringTokenizer(br.readLine());
-			
-			N = Integer.parseInt(st.nextToken()); //재료수
-			L = Integer.parseInt(st.nextToken()); //제한 칼로리
-			
-			ingredients = new int[N][2]; 
-			isUsed = new boolean[N];
-			
-			result = Integer.MIN_VALUE;
-			
-			//각 재료의 평가 및 칼로리
-			for(int i = 0; i<N; i++) {
+
+			N = Integer.parseInt(st.nextToken()); // 재료수
+			L = Integer.parseInt(st.nextToken()); // 제한 칼로리
+
+			scores = new int[N];
+			kcals = new int[N];
+
+			result = 0;
+
+			// 각 재료의 평가 및 칼로리
+			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				int score = Integer.parseInt(st.nextToken());
 				int kcal = Integer.parseInt(st.nextToken());
-				ingredients[i] = new int[] {score, kcal};
+
+				scores[i] = score;
+				kcals[i] = kcal;
 			}
-			
-			//재료를 고르는 개수에 따라 계산
-			for(int i = 1; i<=N; i++) {
-				int[][] hambuger = new int[i][2];
-				MichelinGuide(0, 0, hambuger);
-			}
-			
+
+            //subset
+            MichelinGuide(0, 0, 0);
+
 			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
+        br.close();
 		System.out.println(sb);
 	}
-	
-	static void MichelinGuide(int depth, int start, int[][] hambuger)  {
-		if(depth == hambuger.length) {
-			int kcals = 0;
-			int score = 0;
-			for(int[] arr : hambuger) {
-				score += arr[0];
-				kcals += arr[1];
-			}
-			
-			if(kcals > L) return;
-			else {
-				result = Math.max(result, score);
-			}
-			return;
+
+	static void MichelinGuide(int depth, int score, int kcal) {
+		if (depth >= N) {
+            if(kcal > L) return;
+            
+            if(kcal <= L && score > result){
+            	result = score;
+            }
+            return;
 		}
-		
-		for(int i = start; i<N; i++) {
-			if(!isUsed[i]) {
-				isUsed[i] = true;
-				hambuger[depth] = ingredients[i];
-				MichelinGuide(depth+1, i+1, hambuger);
-				isUsed[i] = false;
-			}
-		}
+
+		// 현재 재료 더하기
+		MichelinGuide(depth + 1, score + scores[depth], kcal + kcals[depth]);
+
+		// 현재 재료 더하기 X
+		MichelinGuide(depth + 1, score, kcal);
 	}
 }
