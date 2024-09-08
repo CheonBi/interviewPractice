36183bdda6abd7781470bb8b3cbdc7af29dcbe8f
CheonBi <122432728+CheonBi@users.noreply.github.com>
2024-09-08 23:13:59 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 322 ms, Memory: 72,880 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,126 +1,111 @@
-/*
- * SWEA No. 1251 하나로
- */
-
 import java.io.*;
 import java.util.*;
 
 public class Solution {
 
+	static int N, T;
+	static PriorityQueue<Edge> pq = new PriorityQueue<>();
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = null;
+		StringTokenizer st;
 
-		int T = Integer.parseInt(br.readLine());
+		T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
 
-			// 섬의 개수
-			int N = Integer.parseInt(br.readLine());
+			int[] arrY = new int[N];
+			int[] arrX = new int[N];
+			int[] parents = new int[N];
+			pq.clear();
 
-			ArrayList<long[]> island = new ArrayList<>();
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				arrX[i] = Integer.parseInt(st.nextToken());
+			}
 
-			// Set graph
-			StringTokenizer Xpoint = new StringTokenizer(br.readLine());
-			StringTokenizer Ypoint = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				arrY[i] = Integer.parseInt(st.nextToken());
+			}
+
 			double E = Double.parseDouble(br.readLine());
+			
 
 			for (int i = 0; i < N; i++) {
+				for (int j = i+1; j < N; j++) {
+					int y1 = arrY[i];
+					int x1 = arrX[i];
 
-				long x = Long.parseLong(Xpoint.nextToken());
-				long y = Long.parseLong(Ypoint.nextToken());
+					int y2 = arrY[j];
+					int x2 = arrX[j];
 
-				island.add(new long[] { y, x });
-			}
-
-			PriorityQueue<Edge> pq = new PriorityQueue<>();
-
-			for (int from = 0; from < N; from++) {
-				long[] A = island.get(from);
-
-				for (int to = from + 1; to < N; to++) {
-					long[] B = island.get(to);
-					long L = getDistance(A, B);
-
-					pq.offer(new Edge(from, to, L));
+					double weight = (Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2)) * E;
+					pq.offer(new Edge(i, j, weight));
 				}
 			}
 
-			int[] parents = new int[N];
+			Arrays.fill(parents, -1);
 
-			// makeset
-			for (int i = 0; i < parents.length; i++) {
-				parents[i] = -1;
-			}
-
-			long weight = 0;
+			double weight = 0;
 			int cnt = 0;
 
-			while (!pq.isEmpty()) {
+			while(!pq.isEmpty()) {
 				Edge edge = pq.poll();
-
-				if (Union(edge.start, edge.end, parents)) {
+				
+				if (Union(edge.from, edge.to, parents)) {
 					weight += edge.weight;
-					if (++cnt == N - 1)
+					if (++cnt == N - 1) {
 						break;
+					}
 				}
-
 			}
-
-			sb.append("#").append(tc).append(" ").append(Math.round(weight * E)).append("\n");
+	
+			sb.append("#").append(tc).append(" ").append(Math.round(weight)).append("\n");
 		}
 
 		System.out.println(sb.toString());
 		br.close();
-
 	}
 
 	static int Find(int N, int[] parents) {
 		if (parents[N] < 0) {
 			return N;
 		}
-		return parents[N] = Find(parents[N], parents); // 경로 압축
+
+		return parents[N] = Find(parents[N], parents);
 	}
 
-	static boolean Union(int x, int y, int[] parents) {
+	static boolean Union(int A, int B, int[] parents) {
+		A = Find(A, parents);
+		B = Find(B, parents);
 
-		int nx = Find(x, parents);
-		int ny = Find(y, parents);
-
-		if (nx == ny) {
+		if (A == B)
 			return false;
-		}
 
-		parents[ny] = nx;
+		parents[A] += parents[B];
+		parents[B] = A;
 
 		return true;
-	}
 
-	static long getDistance(long[] A, long[] B) {
-		long x1 = A[1];
-		long y1 = A[0];
-
-		long x2 = B[1];
-		long y2 = B[0];
-
-		return ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2));
 	}
 
 	static class Edge implements Comparable<Edge> {
-		int start;
-		int end;
-		long weight;
+		int from;
+		int to;
+		double weight;
 
-		public Edge(int start, int end, long weight) {
-			this.start = start;
-			this.end = end;
+		public Edge(int from, int to, double weight) {
+			this.from = from;
+			this.to = to;
 			this.weight = weight;
 		}
 
 		@Override
 		public int compareTo(Edge o) {
-			return Long.compare(this.weight, o.weight);
+			return Double.compare(this.weight, o.weight);
 		}
 	}
 }
