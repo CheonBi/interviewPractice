d1f5aa31c49dbf6812f6946519f02a33bbb53029
CheonBi <122432728+CheonBi@users.noreply.github.com>
2024-09-10 17:11:47 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,7 @@
+SELECT title, a.board_id, reply_id, b.writer_id, b.contents,
+date_format(b.created_date, '%Y-%m-%d') as created_date
+from used_goods_board as a
+join used_goods_reply as b on a.board_id=b.board_id
+where a.created_date like '2022-10%' 
+order by b.created_date, a.title;
+
