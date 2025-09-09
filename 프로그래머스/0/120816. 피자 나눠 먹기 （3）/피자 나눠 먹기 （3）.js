920d917205e86b9b8e65c721531c2d9a9307342a
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-09-09 13:45:52 +0900

    Update 피자 나눠 먹기 （3）.js
    풀이법 추가

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,1 +1,17 @@
+//올림으로 풀기
+//올림으로 나머지 조각해결
 const solution = (slice, n) => Math.ceil(n / slice);
+
+//조각을 더하여 피자 판 맞추기
+const solution = (slice, n) => {
+    let piece = Number(slice);
+    
+    while(true) {
+        if(piece >= n) {
+            break;
+        }
+        piece += slice;
+    }
+    
+    return Math.trunc(piece / slice);
+}
