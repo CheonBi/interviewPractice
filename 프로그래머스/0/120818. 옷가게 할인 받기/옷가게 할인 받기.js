9ecd4f90115280b65f93d31d06b3709f53ae66bf
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-09-09 17:23:45 +0900

    풀이 추가

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -8,3 +8,18 @@ const solution = (price) => {
     
     return price;
 }
+
+
+//for .. of를 활용하여 할인율 배열 순회 계산
+const discounts = [
+    [500000, 20],
+    [300000, 10],
+    [100000,  5],
+]
+
+const solution = (price) => {
+    for (const discount of discounts)
+        if (price >= discount[0])
+            return Math.floor(price - price * discount[1] / 100)
+    return price
+}
