ac7f0c30d3b520ef8854cf2f151bd8dae1a6903a
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-09-09 17:52:30 +0900

    풀이 추가

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,5 +1,20 @@
 const solution = (num_list) => {
     const result = [];
+    
+    for(let i = num_list.length - 1; i>=0; i--) {
+        result.push(num_list[i]);
+    }
+    
+    return result;
+}
+
+//reverse method
+const solution = (num_list) => num_list.reverse();
+
+//with unshift
+//unshift() : 배열의 맨 앞에 값을 추가한다.
+const solution = (num_list) => {
+    const result = [];
 
     num_list.forEach((num) => result.unshift(num));
 
