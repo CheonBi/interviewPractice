919fe64e3e880836bbee20fdffc60bf359acf2b6
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-09-11 17:56:55 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,6 @@
+const solution = (angle) => {
+    if (angle < 90 && angle > 0) return 1;
+    else if (angle === 90) return 2;
+    else if (angle < 180 && angle > 90) return 3;
+    else if (angle === 180) return 4;
+}
