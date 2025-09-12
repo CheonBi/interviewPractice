30747c1b3cc5e1b9642c5c1e9b4869efa7295c8c
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-09-13 01:26:24 +0900

    [level 0] Title: 가위 바위 보, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -4,11 +4,11 @@ const solution = (rsp) => {
     // 0 -> 5
     // 5 -> 2
     
-    const arr = [...rsp];
-    
-    return arr.map((v) => {
-        if(v == '2') return 0;
-        else if(v == '0') return 5;
-        else if(v == '5') return 2;
-    }).join('');
+    let arr = {
+        2 : 0,
+        0 : 5,
+        5 : 2,
+    }
+
+    return [...rsp].map((v) => arr[v]).join('');
 }
