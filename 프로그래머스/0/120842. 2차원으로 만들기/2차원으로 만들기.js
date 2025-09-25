aeb780bbf70650ca9019c870ead951849d4bc4e8
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-09-25 17:11:27 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,16 @@
+const solution = (num_list, n) => {
+    let result = [];
+    for(let i = 0; i < num_list.length;) {
+        let arr = [];
+        
+        for(let j = 0; j<n; j++){
+            arr.push(num_list[i]);
+            i += 1;
+        }
+        
+        result.push(arr);
+
+    }
+    
+    return result;
+}
