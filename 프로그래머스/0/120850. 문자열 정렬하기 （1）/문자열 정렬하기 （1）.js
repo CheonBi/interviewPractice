7747c2433a527e7cc6ac532ec1181b0402b55272
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-10-02 15:49:04 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,4 +1,4 @@
 const solution = (my_string) => {
-    const numbers = my_string.split('').filter((v) => !isNaN(v)).map((v) => parseInt(v));
-    return numbers.sort((a,b) => a - b);
+    const numbers = my_string.split('').filter((v) => !Number.isNaN(Number.parseInt(v, 10)));
+    return numbers.map((v) => Number.parseInt(v, 10)).sort();
 } 
