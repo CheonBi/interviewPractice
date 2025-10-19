f5a257a56a264d7fcda4872085d3506424481473
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-10-20 00:42:44 +0900

    [level 3] Title: 섬 연결하기, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -21,16 +21,13 @@ const solution = (n, costs) => {
     const parents = Array.from({length : n}, (_, i) => i);
     
     //간선 비용 오름차순
-    costs.sort((a,b) => {
-        if(a[2] === b[2]) return a[0] - b[0];
-        return a[2] - b[2];
-    })
+    costs.sort((a,b) => a[2] - b[2]);
     
     //U-F로 인해 이미 같은 집합에 있다면 연결하지 않음
-    for(const cost of costs) {
-        if(find(cost[0], parents) != find(cost[1], parents)) { //연결되어있지 않다면(같은 Root가 아니라면)
-            result += cost[2];
-            union(cost[0], cost[1], parents); //연결
+    for(const [u,v,w] of costs) {
+        if(find(u, parents) != find(v, parents)) { //연결되어있지 않다면(같은 Root가 아니라면)
+            result += w;
+            union(u, v, parents); //연결
         }
     }
 
