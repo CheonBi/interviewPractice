dae0fde023d71e97f3a9e42845380a79825173c9
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-10-19 20:37:15 +0900

    [level 3] Title: 네트워크, Time: 0.43 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,27 +1,37 @@
-function solution(n, computers) {
-    let answer = 0;
-    
-    
-    const length = computers.length;
+const solution = (n, computers) => {
     const visited = Array.from({length : n}, () => false);
-    
-    for(let i = 0; i<length; i++){
-        if(!visited[i]){
-            dfs(i);
-            answer++;
-        }
-    }
-    
-    
-    function dfs(index) {
-        visited[index] = true;
+    let result = 0;
         
-        for(let i = 0; i<length; i++){
-            if(computers[index][i] === 1 && !visited[i]){
-                dfs(i);
+    const bfs = (i) => {
+        const queue = [];
+        
+        let edges = 1;
+        
+        if(!visited[i]) {
+            queue.push(i);
+            visited[i] = true;
+        } else if(visited[i]) {
+            return 0;
+        }
+        
+        
+        while(queue.length > 0) {
+            const ci = queue.shift();
+            for(let d = 0; d < n; d++) {
+                if(!visited[d] && computers[ci][d] === 1) {
+                    visited[d] = true;
+                    queue.push(d);
+                    edges++;
+                }
             }
         }
+        
+        return 1;
     }
     
-    return answer;
+    for(let i = 0; i < n; i++) {
+        result += bfs(i);
+    }
+    
+    return result;
 }
