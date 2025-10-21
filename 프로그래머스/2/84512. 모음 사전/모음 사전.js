38da4b8f975b388b3cf586411c19169a999f8219
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-10-21 23:36:37 +0900

    [level 2] Title: 모음 사전, Time: 1.84 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -2,20 +2,20 @@ const solution = (word) => {
     const vowels = [..."AEIOU"];
     const dict = [];
         
-    const dfs = (depth, str) => {
-        if(depth === str.length) {
+    const mkdict = (length, str) => {
+        if(length === str.length) {
             dict.push(str);
             return;
         }
         
         for(const vowel of vowels) {
-            dfs(depth, str + vowel);
+            mkdict(length, str + vowel);
         }
     }
     
     
     for(let i = 1; i <= 5; i++) {
-        dfs(i, "");
+        mkdict(i, "");
     }
     
     return dict.sort().indexOf(word) + 1;
