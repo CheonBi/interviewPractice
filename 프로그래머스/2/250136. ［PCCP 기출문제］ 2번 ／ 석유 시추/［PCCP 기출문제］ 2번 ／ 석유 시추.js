386338f133df3da515912943ad6f45d0cd4433f1
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-10-22 22:20:04 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 69.20 ms, Memory: 50.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -0,0 +1,59 @@
+const solution = (land) => {
+    const n = land.length;
+    const m = land[0].length;
+    
+    const visited = Array.from({length : n}, () => Array(m).fill(false));
+    const dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]; //상 하 좌 우 [y,x]
+    
+    let result = Array(m).fill(0);
+    
+    const isValid = (ny, nx) => {
+        return ny >= 0 && ny < n && nx >= 0 && nx < m;
+    }
+    
+    
+    const bfs = (start) => {
+        const groups = new Set();
+        const queue = [];
+        
+        queue.push(start);
+        let z = 1;
+        
+        while(queue.length > 0) {
+            const [y,x] = queue.shift();
+            visited[y][x] = true;
+            groups.add(x);
+            
+            for(const [dy, dx] of dir) {
+                const ny = y + dy;
+                const nx = x + dx;
+                
+                if(!isValid(ny,nx)) continue;
+
+                if(!visited[ny][nx] && land[ny][nx] == 1) {
+                    visited[ny][nx] = true;
+                    queue.push([ny, nx]);
+                    z++;
+                }
+            }
+        }
+        
+        for(const idx of groups) {
+            result[idx] += z;
+        }
+        
+        return;
+    }
+    
+    
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            //기름 위치에서 bfs
+            if(land[j][i] === 1 && !visited[j][i]) {
+                bfs([j,i]); 
+            } 
+        }
+    }
+    
+    return Math.max(...result);
+}
