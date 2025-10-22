ce4a42c641dbc4868da04672eb8de5be5cb92648
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-10-22 22:23:27 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 67.38 ms, Memory: 50.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -2,7 +2,7 @@ const solution = (land) => {
     const n = land.length;
     const m = land[0].length;
     
-    const visited = Array.from({length : n}, () => Array(m).fill(false));
+    const visited = Array.from({length : n}, () => Array(m).fill(0));
     const dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]; //상 하 좌 우 [y,x]
     
     let result = Array(m).fill(0);
@@ -21,7 +21,7 @@ const solution = (land) => {
         
         while(queue.length > 0) {
             const [y,x] = queue.shift();
-            visited[y][x] = true;
+            visited[y][x] = 1;
             groups.add(x);
             
             for(const [dy, dx] of dir) {
@@ -30,8 +30,8 @@ const solution = (land) => {
                 
                 if(!isValid(ny,nx)) continue;
 
-                if(!visited[ny][nx] && land[ny][nx] == 1) {
-                    visited[ny][nx] = true;
+                if(visited[ny][nx] === 0 && land[ny][nx] === 1) {
+                    visited[ny][nx] = 1;
                     queue.push([ny, nx]);
                     z++;
                 }
@@ -49,7 +49,7 @@ const solution = (land) => {
     for(let i = 0; i < m; i++) {
         for(let j = 0; j < n; j++) {
             //기름 위치에서 bfs
-            if(land[j][i] === 1 && !visited[j][i]) {
+            if(land[j][i] === 1 && visited[j][i] === 0) {
                 bfs([j,i]); 
             } 
         }
