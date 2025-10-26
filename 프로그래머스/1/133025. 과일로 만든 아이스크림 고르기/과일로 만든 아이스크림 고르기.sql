7ba4a5c6da54952922d1d51ad41966b288c81dec
CheonBi <122432728+CheonBi@users.noreply.github.com>
2025-10-26 15:51:32 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -1,3 +1,5 @@
 select f.flavor
-from first_half as f, icecream_info as i 
-where f.total_order > 3000 and f.flavor = i.flavor and i.ingredient_type = 'fruit_based'
+from first_half as f
+join icecream_info as i on f.flavor = i.flavor
+where f.total_order > 3000 and i.ingredient_type = 'fruit_based'
+order by f.total_order DESC
